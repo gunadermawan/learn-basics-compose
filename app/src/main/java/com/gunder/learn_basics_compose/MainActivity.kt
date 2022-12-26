7efeb081bb65193cce8928dd96da9a8aaddd6aad
gunadermawan <gunadermawan.official@gmail.com>
2022-12-26 23:00:57 +0700

    create lazycolumn and animation

--- app/src/main/java/com/gunder/learn_basics_compose/MainActivity.kt
+++ app/src/main/java/com/gunder/learn_basics_compose/MainActivity.kt
@@ -3,8 +3,13 @@ package com.gunder.learn_basics_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
@@ -56,9 +61,9 @@ fun ComposeTheme() {
 @Composable
 fun GreetingList(name: List<String>) {
     if (name.isNotEmpty()) {
-        Column {
-            for (names in name) {
-                Greeting(name = names)
+        LazyColumn() {
+            items(name) { name ->
+                Greeting(name)
             }
         }
     } else {
@@ -71,6 +76,13 @@ fun Greeting(name: String) {
     var isExpanded by remember {
         mutableStateOf(false)
     }
+    val animateSizeDp by animateDpAsState(
+        if (isExpanded) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
     Row(
         modifier = Modifier.padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
@@ -78,7 +90,7 @@ fun Greeting(name: String) {
         Image(
             painter = painterResource(R.drawable.img),
             contentDescription = "logo jetpack compose",
-            modifier = Modifier.size(80.dp)
+            modifier = Modifier.size(animateSizeDp)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column(modifier = Modifier.weight(1f)) {
