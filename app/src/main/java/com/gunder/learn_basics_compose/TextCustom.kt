3e2e7f2e7f334e4b7a30b4e45136eb9eeb19e72b
gunadermawan <gunadermawan.official@gmail.com>
2023-02-22 22:34:43 +0700

    learn: text alignment

--- app/src/main/java/com/gunder/learn_basics_compose/TextCustom.kt
+++ app/src/main/java/com/gunder/learn_basics_compose/TextCustom.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -16,9 +17,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.gunder.learn_basics_compose.ui.theme.LearnbasicscomposeTheme
 
 class TextCustom : ComponentActivity() {
@@ -38,22 +39,28 @@ class TextCustom : ComponentActivity() {
     }
 }
 
+@Composable
+fun CustomText() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = stringResource(id = R.string.title_activity_text_custom),
+            modifier = Modifier
+                .background(MaterialTheme.colors.primary)
+                .width(200.dp)
+                .padding(16.dp),
+            color = Color.White,
+            fontSize = MaterialTheme.typography.h5.fontSize,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.ExtraBold,
+            textAlign = TextAlign.End
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview2() {
     LearnbasicscomposeTheme {
-        Column(modifier = Modifier.fillMaxSize()) {
-            Text(
-                text = stringResource(id = R.string.title_activity_text_custom),
-                modifier = Modifier
-                    .background(MaterialTheme.colors.primary)
-                    .padding(16.dp),
-                color = Color.White,
-                fontSize = MaterialTheme.typography.h5.fontSize,
-                fontStyle = FontStyle.Italic,
-                fontWeight = FontWeight.ExtraBold
-            )
-        }
+        CustomText()
     }
 }
